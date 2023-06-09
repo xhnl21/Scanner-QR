f6dc931f26e15ccc70d7eee2bacf9f49ac93b722
Xavier <x.nunez@miautobox.com>
2023-06-09 14:14:31 -0400

    agregando transparencia en el body de la vista

--- ScannerQR-V1/src/views/HomePage.vue
+++ ScannerQR-V1/src/views/HomePage.vue
@@ -35,25 +35,31 @@ export default defineComponent ({
   },
   methods: {
         async startScan () {
-          this.showLoading("init 0", true);
+          document.querySelector('body').classList.add('scanner-active');
+          document.getElementById('app').style.display = 'none';
+          
+          // this.showLoading("init 0", true);
           // Check camera permission
           // This is just a simple example, check out the better checks below
           const status = await BarcodeScanner.checkPermission({ force: true });
           if (status.granted) {
+
             // make background of WebView transparent
             // note: if you are using ionic this might not be enough, check below
             BarcodeScanner.hideBackground();
 
             const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
+            
             // if the result has content
             if (result.hasContent) {
               this.showLoading(result.content+2, true);
               console.log(result.content); // log the raw scanned content
               this.text = result.content
+              document.getElementById('app').style.display = 'block';
             }
           } else {
             this.showLoading(status+3, true);
-          }
+          }          
         },
     },
   setup() {
